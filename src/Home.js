3c9c158affd8f1984cfaeaaa2e862ab6a75003a4
Wisdomwarrie11 <wisdomwarrie11@gmail.com>
2024-10-04 15:19:10 +0100

    commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+
+
+const Home = () =>{
+return(
+<>
+<div id='intro' className = 'container'>
+    <div>
+        <h1>FIND YOUR PERFECT SPACE, ROOMMATE AND ESSENTIALS - ALL IN ONE PLACE</h1>
+        <p>University Life Made Easy With CribLink</p>
+    </div>
+
+</div>
+</>
+)
+
+};
+
+export default Home;
