5d8c3656507cd5607f37e46499788c48abb09c6f
Wisdomwarrie11 <wisdomwarrie11@gmail.com>
2024-10-05 13:04:42 +0100

    changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () =>{
         <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
 <div className="container-fluid">
-  <strong><a style = {{color: 'rgb(5, 4, 66)'}} className="navbar-brand" href="/"><span style={{fontSize: '30px'}}>Crib</span>Link</a></strong>
+  <strong><a style = {{color: 'rgb(5, 4, 66)'}} className="navbar-brand" href="/"><span style={{fontSize: '30px'}}>Crib</span>link</a></strong>
   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
