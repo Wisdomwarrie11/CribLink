1e91375da76ccbe36b380939328ad5ccc2911ab8
Wisdomwarrie11 <wisdomwarrie11@gmail.com>
2024-10-06 12:15:35 +0100

    changes in the home page

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -12,28 +12,28 @@ const Carousal = () =>{
   <div className="carousel-inner">
     <div className="carousel-item active">
       <div className="row">
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="room1.jpg" alt="Room 1" className="listing-img" />
             <h3>Spacious Room in Downtown</h3>
             <p>₦20,000 per month</p>
           </div>
         </div>
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="room2.jpg" alt="Room 2" className="listing-img" />
             <h3>Apartment near Campus</h3>
             <p>₦50,000 per month</p>
           </div>
         </div>
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="item1.jpg" alt="Item 1" className="listing-img" />
             <h3>Second-hand Study Desk</h3>
             <p>₦5,000</p>
           </div>
         </div>
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="room2.jpg" alt="Room 3" className="listing-img" />
             <h3>Single Room in Student Hostel</h3>
@@ -45,28 +45,28 @@ const Carousal = () =>{
 
     <div className="carousel-item">
       <div className="row">
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="gen.jpeg" alt="Item 2" className="listing-img" />
             <h3>Second-hand Generator</h3>
             <p>₦30,000</p>
           </div>
         </div>
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="table.jpeg" alt="Room 4" className="listing-img" />
             <h3>Reading Table</h3>
             <p>₦7,000</p>
           </div>
         </div>
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="bucket.jpeg" alt="Item 3" className="listing-img" />
             <h3>Bucket</h3>
             <p>₦1,500</p>
           </div>
         </div>
-        <div className="col-6 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="table.jpeg" alt="Room 5" className="listing-img" />
             <h3>Reading desk</h3>
@@ -107,28 +107,28 @@ const Carousal = () =>{
   <div className="carousel-inner">
     <div className="carousel-item active">
       <div className="row">
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="room1.jpg" alt="Room 1" className="listing-img" />
             <h3>Spacious Room in Downtown</h3>
             <p>₦20,000 per month</p>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="room2.jpg" alt="Room 2" className="listing-img" />
             <h3>Furnished Apartment near Campus</h3>
             <p>₦50,000 per month</p>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="item1.jpg" alt="Item 1" className="listing-img" />
             <h3>Second-hand Study Desk</h3>
             <p>₦5,000</p>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="room2.jpg" alt="Room 3" className="listing-img" />
             <h3>Single Room in Student Hostel</h3>
@@ -140,28 +140,28 @@ const Carousal = () =>{
 
     <div className="carousel-item">
       <div className="row">
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="gen.jpeg" alt="Item 2" className="listing-img" />
             <h3>Second-hand Generator</h3>
             <p>₦30,000</p>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="table.jpeg" alt="Room 4" className="listing-img" />
             <h3>Reading Table</h3>
             <p>₦7,000</p>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="bucket.jpeg" alt="Item 3" className="listing-img" />
             <h3>Bucket</h3>
             <p>₦1,500</p>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-3">
+        <div className="col-3 col-sm-3 col-md-3">
           <div className="listing-card">
             <img src="table.jpeg" alt="Room 5" className="listing-img" />
             <h3>Reading desk</h3>
